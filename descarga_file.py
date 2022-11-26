11b1fb6d063840cbc8b279f3330eba24af903759
Marco Julio Gómez <margomeza16@gmail.com>
2022-11-26 23:10:34 +0000

    Se modifica archivo descarga_file.py
    git status

--- descarga_file.py
+++ descarga_file.py
@@ -1,6 +1,9 @@
 import os
 import zipfile
+
 def descarga_f(orig, destino):
 #Descomprime archivo, cuya ruta y nombre se indica en el parametro orig y lo deja en la ruta /tmp/destino/
-    unzip orig -d /tmp/destino/
+    #unzip orig -d /tmp/destino/
+    zip = zipfile.ZipFile(orig,'r') 
+    zip.extractall(destino)
     return 0  
